7556497337dd3cc5de9ef70d6d504612e700b14a
lgy <lgy@masa.local>
2022-11-02 17:00:44 +0100

    add clang-format configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,35 @@
 #include <oatpp/web/server/HttpConnectionHandler.hpp>
 
 void run() {
-
   /* Create Router for HTTP requests routing */
   auto router = oatpp::web::server::HttpRouter::createShared();
 
   /* Create HTTP connection handler with router */
-  auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
+  auto connectionHandler =
+      oatpp::web::server::HttpConnectionHandler::createShared(router);
 
   /* Create TCP connection provider */
-  auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8000, oatpp::network::Address::IP_4});
+  auto connectionProvider =
+      oatpp::network::tcp::server::ConnectionProvider::createShared(
+          {"localhost", 8000, oatpp::network::Address::IP_4});
 
-  /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
+  /* Create server which takes provided TCP connections and passes them to HTTP
+   * connection handler */
   oatpp::network::Server server(connectionProvider, connectionHandler);
 
   /* Print info about server port */
-  OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
+  OATPP_LOGI("MyApp", "Server running on port %s",
+             connectionProvider->getProperty("port").getData());
 
   /* Run server */
   server.run();
 }
 
 int main() {
-    oatpp::base::Environment::init();
+  oatpp::base::Environment::init();
 
-    run();
+  run();
 
-    oatpp::base::Environment::destroy();
-    return 0;
+  oatpp::base::Environment::destroy();
+  return 0;
 }
